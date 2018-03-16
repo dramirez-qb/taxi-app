9af45c1c3a2845b59884f4d4d30060c3d264f9f1
Jason Parent <jason.a.parent@gmail.com>
2018-03-15 23:37:52 -0400

    Removes unnecessary code

--- taxi-ui/src/app/app.component.ts
+++ taxi-ui/src/app/app.component.ts
@@ -1,19 +1,10 @@
-import { Component, ViewContainerRef } from '@angular/core';
-
-import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'app';
-  constructor(
-    private toastsManager: ToastsManager,
-    private viewContainerRef: ViewContainerRef
-  ) {
-    this.toastsManager.setRootViewContainerRef(viewContainerRef);
-  }
 }
