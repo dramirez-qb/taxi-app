96c8b705b4e97eb86fefda6ce2c4d8a71674c2e5
Jason Parent <jason.a.parent@gmail.com>
2018-09-15 10:22:51 -0400

    Removes old directory

--- django-example-taxi/example_taxi/example/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .apis import TripView
-
-urlpatterns = [
-    url(r'^$', TripView.as_view({'get': 'list'}), name='trip_list'),
-    url(r'^(?P<trip_nk>\w{32})/$', TripView.as_view({'get': 'retrieve'}), name='trip_detail'),
-]
--- django-example-taxi/example_taxi/example_taxi/routing.py
+++
@@ -1,8 +0,0 @@
-from channels import route_class
-from example.consumers import DriverConsumer, RiderConsumer
-
-
-channel_routing = [
-    route_class(DriverConsumer, path=r'^/driver/$'),
-    route_class(RiderConsumer, path=r'^/rider/$'),
-]
